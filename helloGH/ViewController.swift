6b1090d912ce38eba714087217c5db01fffa49f6
wen-viv <vivianzhu@vivians-macbook-air.local>
2016-09-05 13:55:44 -0700

    Initial Commit

---
+++ helloGH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloGH
+//
+//  Created by Vivian Zhu on 9/5/16.
+//  Copyright © 2016 Vivian Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
